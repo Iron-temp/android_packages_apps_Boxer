e849124d20df7e8451427fca06d1dbbad43b45e5
Alex <du.alexcruz@gmail.com>
2020-02-20 12:35:22 -0500

    D-House: Navbar: Add navbar layout preference
    Change-Id: Id092c83daa490f29e04bc5b88e52508b1dc53f9c
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/org/boxer/iron/fragments/Buttons.java
+++ src/org/boxer/iron/fragments/Buttons.java
@@ -54,6 +54,7 @@ import org.boxer.iron.preference.ActionFragment;
 public class Buttons extends ActionFragment implements OnPreferenceChangeListener {
     private static final String HWKEY_DISABLE = "hardware_keys_disable";
     private static final String KEY_NAVIGATION_BAR_ENABLED = "force_show_navbar";
+    private static final String KEY_LAYOUT_SETTINGS = "layout_settings";
 
     // category keys
     private static final String CATEGORY_HWKEY = "hardware_keys";
@@ -172,6 +173,8 @@ public class Buttons extends ActionFragment implements OnPreferenceChangeListene
                 Settings.System.FORCE_SHOW_NAVBAR,
                 defaultToNavigationBar ? 1 : 0) == 1));
         mNavigationBar.setOnPreferenceChangeListener(this);
+
+        Preference mLayoutSettings = (Preference) findPreference(KEY_LAYOUT_SETTINGS);
     }
 
     @Override
