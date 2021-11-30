811f9d724ef7dc936f37344778030fd2bd792580
ralf979 <ralf979@gmail.com>
2021-11-30 15:56:41 +0000

    Welcome Dog House !!!!
    Thank you very much to AOSP Extended, this only starts because of their work.
    so all credits are from AEX team

---
+++ src/org/boxer/iron/navigation/BubbleNavigationChangeListener.java
@@ -0,0 +1,7 @@
+package org.boxer.iron.navigation;
+
+import android.view.View;
+
+public interface BubbleNavigationChangeListener {
+    void onNavigationChanged(View view, int position);
+}
---
+++ src/org/boxer/iron/navigation/IBubbleNavigation.java
@@ -0,0 +1,15 @@
+package org.boxer.iron.navigation;
+
+import android.graphics.Typeface;
+import org.boxer.iron.navigation.BubbleNavigationChangeListener;
+
+@SuppressWarnings("unused")
+public interface IBubbleNavigation {
+    void setNavigationChangeListener(BubbleNavigationChangeListener navigationChangeListener);
+
+    void setTypeface(Typeface typeface);
+
+    int getCurrentActiveItemPosition();
+
+    void setCurrentActiveItem(int position);
+}
